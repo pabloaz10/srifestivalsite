a59f6a4ab78a1de5f4c8046518fb89b5c5c9417e
Pabloaz10 <pablo.pmza@gmail.com>
2025-10-02 16:29:06 -0300

    Update Hero.tsx
    add botton inscrição

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import Invitation from './Invitation';
 const Hero: React.FC = () => {
   // Lógica do botão de inscrição
   const handleButtonClick = () => {
-    window.open('https://forms.office.com/pages/responsepage.aspx?id=cYIpl9cbxUqTW4it3vY2zGmXMZhPMuVGt3o3ldp5851UN1RLSTdCVDZGQjhXWkZST0pWV0ZJTElTTS4u', '_blank');
+    window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=cYIpl9cbxUqTW4it3vY2zDPil7ZNfxhOsdONGXuqCUJUQVFJNVlLOENWNkhVSlc0VFJSTFdSOEJFSy4u ');
   };
 
   return (
@@ -22,20 +22,25 @@ const Hero: React.FC = () => {
           </video>
         </div>
         <div className="absolute inset-0 bg-[#193869] opacity-10 z-10"></div>
-        <div className="relative z-20 text-left ml-74 mt-26 max-w-3xl">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bebas uppercase tracking-normal text-white leading-18">
+        <div className="relative z-20 text-left px-4 sm:px-6 lg:ml-74 lg:mt-26 max-w-3xl">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-bebas uppercase tracking-normal text-white leading-tight sm:leading-tight md:leading-tight lg:leading-18">
             O maior evento de <br />democratização da <br />inovação está chegando!
           </h1>
-          <h2 className="font-bebas mt-4 text-8xl sm:text-6xl md:text-8xl font-medium text-cyan-300">
+          <h2 className="font-bebas mt-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-medium text-cyan-300">
             21 e 22 de Outubro
           </h2>
-          <div className="mt-8 inline-block">
-            <span className="bg-[#E40088] text-white text-4xl sm:text-4xl font-bold font-bebas py-1 px-10 shadow-lg">
+          <div className="mt-6 sm:mt-8 inline-block">
+            <span className="bg-[#E40088] text-white text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold font-bebas py-2 px-4 sm:py-2 sm:px-6 md:py-3 md:px-8 lg:px-10 shadow-lg">
               2 DIAS DE EVENTO GRATUITO
             </span>
           </div>
-          <img
-            className="absolute z-30 top-150 right-[-156%] opacity-20 xl:top-[-100px] transform scale-180"
+          <button
+            onClick={handleButtonClick}
+            className="bg-[#37e3f0] text-black flex mt-6 sm:mt-8 md:mt-10 text-lg sm:text-xl font-bold py-3 px-6 sm:py-4 sm:px-8 hover:bg-[#04f7ff] transition-colors cursor-pointer shadow-lg hover:scale-105 transform duration-200"
+          >
+            Inscreva-se
+          </button>
+          <img className="absolute z-30 top-150 right-[-156%] opacity-20 xl:top-[-100px] transform scale-180 hidden lg:block"
             src={'/assets/svg/map.svg'}
             alt="Map SVG - SRI"
           />
