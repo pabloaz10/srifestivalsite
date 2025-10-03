d560b1b738519680f1e38a4fc3dd80e8c4b42a2d
Pabloaz10 <pablo.pmza@gmail.com>
2025-10-03 11:38:48 -0300

    Update lineup.ts

--- src/mock/lineup.ts
+++ src/mock/lineup.ts
@@ -23,18 +23,6 @@ export const mockLineup = [
     link: "https://linkedin.com/in/igor-coelho",
     link_name: "Conectar",
     status: "destaque"
-  },
-  {
-    id: "3", 
-    name: "Igor Coelho",
-    time: "19:00:00",
-    date: "2025-10-22",
-    pic: "/assets/palestrantes/Igor-Coelho.jpeg",
-    bio: "Igor Coelho, conhecido também como Igor 3K, nascido no Rio de Janeiro, é fundador do Grupo Flow, co-fundador e apresentador do Flow Podcast. Está na internet há mais de 10 anos, e antes de chegar ao seu ápice, como apresentador do Flow Podcast, foi criador de conteúdo sobre games, onde se tornou grande referência para o cenário de Fighting Games no Brasil. Segundo ele, sua capacidade de se comunicar com qualquer pessoa é uma herança de seu pai. Ele acredita que ter um interesse sincero pelo que as pessoas têm a dizer é crucial, assim como a habilidade de se adaptar a diferentes situações em uma conversa, o que ele credita como parte de seu sucesso no mundo dos vídeos e podcasts no Brasil.",
-    title: "Palestrante, Apresentador e Criador de Conteúdo",
-    link: "https://linkedin.com/in/igor-coelho",
-    link_name: "Conectar",
-    status: "published"
   }
 ];
 
